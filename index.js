786d85fe56afb526e5734380207310ff652ed965
Roro Bodkins <gitopwtd.xfnfr@slmail.me>
2022-09-08 03:57:34 +0200

    Implement all the tasks

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const lastOperation = document.querySelector('.last-operation');
 const currentOperation = document.querySelector('.current-operation');
 const operators = document.querySelectorAll('.operator');
 const operands = document.querySelectorAll('.operand');
-// const decimalButton = document.querySelector('.decimal-button');
+const decimalButton = document.querySelector('.decimal-button');
 const equalButton = document.querySelector('.equal-button');
 const clearButton = document.querySelector('.clear-button');
 
 
 // LET VARIABLES
-let a = 0;
-let b = 0;
-let isOperator;
+let a = "";
+let b = "";
+let secondOperation;
 let myOperator;
 let result;
-let isDecimal;
+let decimalState = true;
+let infinitum;
 
 
 // var displayValue = 0;
@@ -23,85 +24,152 @@ let isDecimal;
 
 // FUNCTION
 const saveOperand = (e) => {
-  // if(e.target.textContent === "." && isOperator) {
+  if(!a && a !== 0 || !secondOperation) {    
+    a += e.target.textContent;
+    currentOperation.textContent = parseFloat(a, 10); // DISPLAY
+    console.log(`a = ${a}`);
+  } else if(!b || secondOperation) {            
+      b += e.target.textContent;
+      currentOperation.textContent = parseFloat(b, 10); // DISPLAY
+      console.log(`b = ${b}`);    
+      operate(parseFloat(a), parseFloat(b), translateOperator(myOperator)); // OPERATION
+ }
+  // if(!a || decimalState === true) {   
+  //   secondOperation = true; 
+  //   a += e.target.textContent;
+  //   currentOperation.textContent = a;
+  //   console.log(a);
+  // } else if(e.target.textContent === "." && secondOperation) {
+  //   console.log(`decimal and secondOperation ${secondOperation}`)
   //   a += ".";
-  // } else if (e.target.textContent === "." && !isOperator) {
+  //   decimalState = true;
+  //   console.log(`new a = ${a}`);
+  // } else if(e.target.textContent === "." && !secondOperation) {
+  //   console.log(`decimalState: ${decimalState}`)
+  //   console.log(`decimal and secondOperation ${secondOperation}`)
   //   b += ".";
-  // }
-
-  if(!a || isDecimal === true) {
-    isOperator = true;
-    a += parseFloat(e.target.textContent);
-    currentOperation.textContent = a;
-    console.log(a);
-  } else if(e.target.textContent === "." && isOperator) {
-    console.log(`decimal and isOperator ${isOperator}`)
-    a += ".";
-    isDecimal = true;
-    console.log(`new a = ${a}`);
-  } else if(!b) {
-    if(isDecimal) {
-      a += e.target.textContent;
-      return;
-    }
-
-    isOperator = false;
-    b = e.target.textContent;
-    currentOperation.textContent = b;
-    console.log(b);    
-    operate(parseFloat(a), parseFloat(b), translateOperator(myOperator));
-    // Remember that operate() doesn't return a result
-    // Do not console.log(result)
-    // Go to operate()
-  } else if(e.target.textContent === "." && !isOperator) {
-    console.log(`decimal and isOperator ${isOperator}`)
-    b += ".";
-  }
+  //   currentOperation.textContent = b;
+  //   console.log(`b at the last else if: ${b}`);
+  // } else if(!b || decimalState === false) {
+  //   if(decimalState) {
+  //     a += e.target.textContent;
+  //     return;
+  //   } 
+    
+  //   secondOperation = false;
+  //   b += parseFloat(e.target.textContent);
+  //   currentOperation.textContent = b;
+  //   console.log(b);    
+  //   operate(parseFloat(a), parseFloat(b), translateOperator(myOperator));
+  
+  //   // Remember that operate() doesn't return a result
+  //   // Do not console.log(result)
+  //   // Go to operate()
+  // } 
 };
 
 
+
+const clearCalc = () => {
+  if (infinitum) {
+    currentOperation.classList.remove("we-do-not");
+    operators.forEach(operator => {
+      operator.classList.remove("operator-disabled");
+    });
+    equalButton.classList.remove("operator-disabled");
+    infinitum = false;
+  }
+  
+  lastOperation.textContent = "";
+  currentOperation.textContent = "";
+ 
+  a = "";
+  secondOperation = false;
+  b = "";
+  result = 0;
+  // decimalState = true;
+}
+
+
+const dividedByZero = () => {
+  currentOperation.classList.add("we-do-not");
+  currentOperation.textContent = `We Don't Do That Here`;
+  operators.forEach(operator => {
+    operator.classList.add("operator-disabled");
+  });
+  equalButton.classList.add("operator-disabled");
+  infinitum = true;
+}
+
+///////////////////
 // EVENT LISTENERS
+//////////////////
 operands.forEach(operand => 
   operand.addEventListener('click', saveOperand));
 
 operators.forEach(operator =>
-  operator.addEventListener('click', (e) => {
-    if (isOperator) {
+  operator.addEventListener('click', (e) => {    
+    
+    if (!secondOperation) {
       myOperator = e.target.textContent;
       console.log(myOperator);
-      isDecimal = false;   
+      secondOperation = true;
+         
     } else {
-      myOperator = e.target.textContent;
-      console.log(myOperator);    
-      currentOperation.textContent = result;
-      a = result;
-      b = null;
-      isOperator = true;
-      isDecimal = false;
+        myOperator = e.target.textContent;
+        console.log(myOperator);    
+        currentOperation.textContent = result;
+        result ? a = result : a = 0;      
+        console.log(`result = ${result} and a = ${a}`); 
+        b = "";       
+        
     }   
 }));
 
 
-// decimalButton.addEventListener('click', () => {
-  
-// });
+decimalButton.addEventListener('click', () => {
+  if (!secondOperation) {
+    a += ".";
+    currentOperation.textContent = parseFloat(a, 10); // DISPLAY
+    console.log(`a = ${a}`);
+  } else {
+    b += ".";
+    currentOperation.textContent = parseFloat(b, 10); // DISPLAY
+    console.log(`b = ${b}`);
+  }
+});
+
+
+// operators.forEach(operator =>
+//   operator.addEventListener('click', (e) => {    
+//     if (secondOperation) {
+//       myOperator = e.target.textContent;
+//       console.log(myOperator);
+//       decimalState = false;   
+//     } else {
+//         myOperator = e.target.textContent;
+//         console.log(myOperator);    
+//         currentOperation.textContent = result;
+//         result ? a = result : a = "";       
+//         b = null;
+//         secondOperation = true;
+//         decimalState = false;
+//     }   
+// }));
+
+
+
 
 
 equalButton.addEventListener('click', () => {
   a = result;
-  b = null;
+  b = "";
   currentOperation.textContent = result;
 });
 
 
-clearButton.addEventListener('click', () => {
-  lastOperation.textContent = "";
-  currentOperation.textContent = "";
-  a = 0;
-  b = 0;
-  result = 0;
-});
 
+clearButton.addEventListener('click', clearCalc);
 
 
 
@@ -119,11 +187,16 @@ const subtract = (a, b) => {
 }
 
 const multiply = (a, b) => {
-  return a * b;
+  return Number((a * b).toFixed(4));
 }
 
 const divide = (a, b) => {
-  return a / b;
+  if(b === 0) {
+    dividedByZero();         
+    return;
+  } 
+  
+  return Number((a / b).toFixed(4));  
 }
 
 const modulo = (a, b) => {
