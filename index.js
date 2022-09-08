f8eae5eb520d0971e72bfb3db02d901fb462e0c4
Roro Bodkins <gitopwtd.xfnfr@slmail.me>
2022-09-08 15:27:43 +0200

    Implement keyboard support for equal, multiply, add, subtract and divide operations

--- index.js
+++ index.js
@@ -22,12 +22,31 @@ let equalPressed;
 
 // FUNCTIONS
 const saveOperand = (e) => {
-  if(!a && a !== 0 || !secondOperation) {    
-    a += e.target.textContent;
+  console.log(`secondOperation at the start of saveOperand = ${secondOperation}`);
+  let helper = e;
+  if (e.type === 'click') {
+    helper = e.target.dataset.key;
+  } else {
+    helper = e.key;
+  }
+
+  if (b === "" && equalPressed === true) { // If we input after after equal signed was pressed.
+    secondOperation = false;               // We "reset" and start receiving 'a' input
+    a = helper;
+    currentOperation.textContent = a;
+    equalPressed = false;
+  }
+
+
+  else if(!a && a !== 0 || !secondOperation) {           
+    // a += e.target.textContent;
+    a += helper;
+    // console.log(`A is ${a}`);
     currentOperation.textContent = parseFloat(a, 10); // DISPLAY
     console.log(`a = ${a}`);
   } else if(!b || secondOperation) {            
-      b += e.target.textContent;
+      // b += e.target.textContent;
+      b += helper;
       currentOperation.textContent = parseFloat(b, 10); // DISPLAY
       console.log(`b = ${b}`);    
       operate(parseFloat(a), parseFloat(b), translateOperator(myOperator)); // OPERATION
@@ -79,16 +98,17 @@ const dividedByZero = () => {
 function translateOperator(operator) {
   if(operator === '+') {
     return add;
-  } else if(operator === '−') {
+  } else if(operator === '-') {
     return subtract;
-  } else if(operator === 'x') { 
+  } else if(operator === 'x' || operator === '*') { 
     return multiply;
-  } else if(operator === '÷') {
+  } else if(operator === '/') {
     return divide;
   } else {
     return modulo;
   }
 }
+
 
 function operate (a, b, operator) {  
   result = operator(a,b);  
@@ -96,29 +116,71 @@ function operate (a, b, operator) {
 }
 
 
+function inputOperator(e) {
+  console.log(`secondOperation at the start of inputOperation = ${secondOperation}`);
+  let helper = e;
+  if (e.type === 'click') {
+    helper = e.target.dataset.key;
+  } else {
+    helper = e.key;
+  }  
+  
+  if (!secondOperation) {
+    myOperator = helper;
+    console.log(myOperator);
+    secondOperation = true;
+    
+       
+  } else {
+      myOperator = helper;
+      console.log(myOperator);    
+      currentOperation.textContent = result;
+      result ? a = result : a = 0;      
+      console.log(`result = ${result} and a = ${a}`); 
+      b = ""; 
+      console.log(`b after the 1st operator = ${b}`);
+  }
+  equalPressed = false;
+}
+
+function inputEqual(e) {
+  if (a === "" || b === "") { // If a or b are not defined yet
+    console.log("undefined");
+    return;
+  } 
+  // secondOperation = false;
+  a = result;    
+  console.log(`after equalButton, a = ${a}`)
+  b = "";
+  console.log(`after equalButton, b = ${b}`)
+  currentOperation.textContent = result;
+  equalPressed = true;
+  console.log(`equalPressed = ${equalPressed}`);
+}
 
 // EVENT LISTENERS
 operands.forEach(operand => 
   operand.addEventListener('click', saveOperand));
 
-operators.forEach(operator =>
-  operator.addEventListener('click', (e) => {    
-    
-    if (!secondOperation) {
-      myOperator = e.target.textContent;
-      console.log(myOperator);
-      secondOperation = true;
+operators.forEach(operator =>  
+  // operator.addEventListener('click', (e) => {    
+    operator.addEventListener('click', inputOperator));
+  //   if (!secondOperation) {
+  //     myOperator = e.target.textContent;
+  //     console.log(myOperator);
+  //     secondOperation = true;
          
-    } else {
-        myOperator = e.target.textContent;
-        console.log(myOperator);    
-        currentOperation.textContent = result;
-        result ? a = result : a = 0;      
-        console.log(`result = ${result} and a = ${a}`); 
-        b = "";       
+  //   } else {
+  //       myOperator = e.target.textContent;
+  //       console.log(myOperator);    
+  //       currentOperation.textContent = result;
+  //       result ? a = result : a = 0;      
+  //       console.log(`result = ${result} and a = ${a}`); 
+  //       b = "";       
         
-    }   
-}));
+  //   }
+//   // }
+// ));
 
 
 decimalButton.addEventListener('click', () => {
@@ -150,21 +212,55 @@ decimalButton.addEventListener('click', () => {
 });
 
 
-equalButton.addEventListener('click', () => {
-  if (a === "" || b === "") { // If a or b are not defined yet
-    console.log("undefined");
-    return;
-  } 
-  // secondOperation = false;
-  a = result;    
-  b = "";
-  currentOperation.textContent = result;
-  equalPressed = true;
-});
+
+equalButton.addEventListener('click', inputEqual);
+
+
+
+// equalButton.addEventListener('click', () => {
+//   if (a === "" || b === "") { // If a or b are not defined yet
+//     console.log("undefined");
+//     return;
+//   } 
+//   // secondOperation = false;
+//   a = result;    
+//   console.log(`after equalButton, a = ${a}`)
+//   b = "";
+//   console.log(`after equalButton, b = ${b}`)
+//   currentOperation.textContent = result;
+//   equalPressed = true;
+// });
 
 
 clearButton.addEventListener('click', clearCalc);
 
+
+// KEYBOARD EVENT LISTENER
+window.addEventListener('keydown', e =>
+{
+    keyboardEvents(e);    
+    // console.log(e.key);
+})
+
+function keyboardEvents(e)
+{
+    if(e.key >= 0 && e.key <= 9) {
+      // console.log(e.key);
+      saveOperand(e);
+    } 
+    else if(e.key === '+' || e.key === '-' || e.key === '*'  || e.key === '/' || e.key === '%') {      
+      inputOperator(e);
+    } 
+    // else if(key === 'Backspace') {
+
+    // }
+    else if(e.key === 'Enter' || e.key === '=') {
+      inputEqual(e);
+    }
+    else if(e.key === 'Delete') {
+       
+    }
+}
 
 
 // OPERATORS
